cf67b83fa65538567129a2ee1c133fed51a2113c
gulzhantulbayeva <122427091+gulzhantulbayeva@users.noreply.github.com>
2023-02-28 23:06:44 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+//1.	Write a function that prints “Hello, World!” three times (line by line).
+for(let a=0;a<4;a++);{
+    console.log("Hello,World") 
+}
+
+
+//2.	Write a function that finds a perimeter of the rectangle given by two sides.
+
+function myFunction(a, b) {
+  return 2*(a + b); 
+            
+}
+
+
+//9.	Write a function to check whether a given number is positive or negative.
+
+let a=-5;
+function Check(a){
+    if(a>0) {console.log("The given number is positive");}
+
+    if(a<0){
+        console.log("The given number is negative ");
+    }
+
+}
